ec733cfbb331e72917166d6f06f38523e8f3bee3
AlexSvet117 <alex.svet117@gmail.com>
2025-04-07 13:32:12 +0100

    get gook by id

--- controllers/book_controller.py
+++ controllers/book_controller.py
@@ -9,3 +9,10 @@ book_bp = Blueprint("books", __name__, url_prefix="/v2")
 def get_books():
     books = BookService.get_all_books()
     return jsonify([book.to_dict() for book in books])
+
+@book_bp.route("/books/<string:book_id>", methods = ["GET"])
+def get_book(book_id):
+    book = BookService.get_product_by_id(book_id)
+    if book:
+        return jsonify(book.to_dict())
+    return jsonify({"error" : "Book not found"}), 404
--- services/book_service.py
+++ services/book_service.py
@@ -4,3 +4,10 @@ class BookService:
     @staticmethod
     def get_all_books():
         return book_data
+    
+    @staticmethod
+    def get_product_by_id(book_id :str):
+        for book in book_data:
+            if book.book_id == book_id:
+                return book
+        return None
